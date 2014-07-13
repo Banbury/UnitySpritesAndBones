6b33d4b90caad05b8c68f45dc0c85092e485c09e
Christian "ketura" McCarty <hyperlink.teltura@gmail.com>
2014-07-13 14:47:56 -0600

    Updating helper menu item to conform to the new menu setup.

--- Assets/SpritesAndBones/Scripts/Helper.cs
+++ Assets/SpritesAndBones/Scripts/Helper.cs
@@ -70,7 +70,7 @@ public class Helper : MonoBehaviour
 	private float cameraSize = 0;
 
 #if UNITY_EDITOR
-	[MenuItem("GameObject/Create Other/Helper")]
+	[MenuItem("Sprite And Bones/Helper")]
 	public static void Create()
 	{
 		GameObject o = new GameObject("Helper");
