bf3d040c43999532df7de18c2c7a7939ee4ecc91
Muhammad Umar <50618335+mianumar@users.noreply.github.com>
2023-07-24 13:20:21 +0500

    Create HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,11 @@
+using System;
+ 
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
